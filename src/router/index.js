3f97cf3a3d5c27d05c5bcb10a83fabf8dd7497d5
Will Miller <will.miller@Wills-MacBook-Pro-2.local>
2019-03-29 12:56:10 -0500

    Connected xAPI to firebase user

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import firebase from 'firebase'
 
+// Import views
 import Lesson from '@/views/Lesson'
 import Login from '@/views/Login'
 import Account from '@/views/Account'
 
 Vue.use(Router)
 
+// Setup routes
 const router = new Router({
   mode: 'history',
   routes: [{
@@ -38,6 +40,7 @@ const router = new Router({
   ]
 })
 
+// Before routing, check auth
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
   const currentUser = firebase.auth().currentUser
--- src/xAPI.js
+++ src/xAPI.js
@@ -1,6 +1,7 @@
 /* global ADL */
 
 import EventBus from './EventBus'
+const fb = require('./firebase.js')
 
 const xAPI = {
   setEvents(events) {
@@ -10,7 +11,7 @@ const xAPI = {
     // eslint-disable-next-line
     console.log(vueObject);
     const stmt = new ADL.XAPIStatement(
-      new ADL.XAPIStatement.Agent('mailto:emailaddress@example.com', 'Email Address'),
+      new ADL.XAPIStatement.Agent('mailto:' + fb.auth.currentUser.email, 'Email Address'),
       new ADL.XAPIStatement.Verb('http://adlnet.gov/expapi/verbs/completed', 'completed'),
       new ADL.XAPIStatement.Activity('http://vwf.adlnet.gov/xapi/virtual_world_sandbox', 'candlestick activity row')
     );
